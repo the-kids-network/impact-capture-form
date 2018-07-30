531de57ebe6e312d48df13654dbd1c48e4bd8dee
Ian Robinson <ianrob1201@gmail.com>
2018-07-30 21:26:01 +0100

    Added tests for all pages
    Also starting to add circleci config

--- app/Http/Middleware/VerifyManager.php
+++ app/Http/Middleware/VerifyManager.php
@@ -16,7 +16,7 @@ class VerifyManager
      */
     public function handle($request, Closure $next)
     {
-        if ($request->user() && ($request->user()->isManager() || $request->user()->isAdmin() || Spark::developer($request->user()->email)) ) {
+        if ($request->user() && ($request->user()->isManager() /*|| $request->user()->isAdmin()*/ || Spark::developer($request->user()->email)) ) {
             return $next($request);
         }
 
--- tests/Feature/PageAuthenticationTest.php
+++ tests/Feature/PageAuthenticationTest.php
@@ -16,26 +16,26 @@ class PageAuthenticationTest extends TestCase
     public function testBasicPageAuthentication()
     {
         // Test authentication via status codes on various pages:
-        //                      Page    , Guest , Mentor, Manager   , Admin
-        $this->multiplePages([['/'      , 200   , 200   , 200       , 200],
-                              ['/home'  , 302   , 200   , 200       , 200],
-                              ['/login' , 200   , 302   , 302       , 302],
-                              ['/report', 302   , 302   , 302       , 200],
+        //                      Page                            , Guest , Mentor, Manager   , Admin
+        $this->multiplePages([['/'                              , 200   , 200   , 200       , 200],
+                              ['/home'                          , 302   , 200   , 200       , 200],
+                              ['/login'                         , 200   , 302   , 302       , 302],
+                              ['/report'                        , 302   , 302   , 302       , 200],
 
-                              ['/activity-type'  , 302   , 302   , 302       , 200],
-                              ['/mentee'  , 302   , 302   , 302       , 200],
-                              ['/physical-appearance'  , 302   , 302   , 302       , 200],
-                              ['/emotional-state'  , 302   , 302   , 302       , 200],
-                              ['/expense-claim'  , 302   , 302   , 302       , 200],
-                              ['/roles/mentor'  , 302   , 302   , 302       , 200],
-                              ['/roles/manager'  , 302   , 302   , 302       , 200],
-                              ['/roles/finance'  , 302   , 302   , 302       , 200],
-                              ['/roles/admin'  , 302   , 302   , 302       , 200],
-                              ['/register'  , 302   , 302   , 302       , 200],
-                              ['/my-reports'  , 302   , 200   , 404       , 404],
-                              ['/my-expense-claims'  , 302   , 200   , 404       , 404],
-                              ['/manager/review-claims'  , 404   , 404   , 200       , 200],
-                              ['/manager/report/export'  , 404   , 404   , 200       , 200],
+                              ['/activity-type'                 , 302   , 302   , 302       , 200],
+                              ['/mentee'                        , 302   , 302   , 302       , 200],
+                              ['/physical-appearance'           , 302   , 302   , 302       , 200],
+                              ['/emotional-state'               , 302   , 302   , 302       , 200],
+                              ['/expense-claim'                 , 302   , 302   , 302       , 200],
+                              ['/roles/mentor'                  , 302   , 302   , 302       , 200],
+                              ['/roles/manager'                 , 302   , 302   , 302       , 200],
+                              ['/roles/finance'                 , 302   , 302   , 302       , 200],
+                              ['/roles/admin'                   , 302   , 302   , 302       , 200],
+                              ['/register'                      , 302   , 302   , 302       , 200],
+                              ['/my-reports'                    , 302   , 200   , 404       , 404],
+                              ['/my-expense-claims'             , 302   , 200   , 404       , 404],
+                              ['/manager/review-claims'         , 404   , 404   , 200       , 200],
+                              ['/manager/report/export'         , 404   , 404   , 200       , 200],
                               ['/manager/expense-claim/export'  , 404   , 404   , 200       , 200]
                               //TODO: Manager and mentor roles
                               //TODO: DELETE ALL - Admin only
