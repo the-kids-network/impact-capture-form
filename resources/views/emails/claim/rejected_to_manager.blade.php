c863bac3fdf50e88e25e9995c6c38ee3f59f057f
Vijay Patel <veejspeej@gmail.com>
2019-08-05 13:34:33 +0100

    feat(expenses): Removes emails to manager and admin as they are not valuable to TKN and generate a lot of noise.

--- resources/views/emails/claim/rejected_to_manager.blade.php
+++
@@ -1,9 +0,0 @@
-@component('mail::message')
-
-# Expense Claim Rejected
-
-The expense claim submitted on {{ $claim->created_at->toFormattedDateString() }} by {{  $claim->mentor->name }} was rejected.
-
-Thanks,<br>
-{{ config('app.name') }}
-@endcomponent
