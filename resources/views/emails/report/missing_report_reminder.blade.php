f9842f64196f83e2072ecfb8bce3be6bd5120f0a
Vijay Patel <veejspeej@gmail.com>
2019-08-16 20:19:04 +0100

    feat(report-reminder): Changes copy and adds link to edit the planned session directly from the email for convenience.

--- resources/views/emails/report/missing_report_reminder.blade.php
+++ resources/views/emails/report/missing_report_reminder.blade.php
@@ -5,12 +5,18 @@ Hello,
 
 We haven’t yet received a report from your planned session with {{ $mentee->name }} on {{ $plannedSession->date->toFormattedDateString() }}. <b>Please do this within the next working day.</b>
 
-As part of your role, this needs to be submitted within 3 days of each meeting with your mentee. If your session hasn’t happened for any reason, please get in touch with the team to make them aware.
+As part of your role, this needs to be submitted within 3 days of each meeting with your mentee. 
+
+If the planned session changed or did not happen, you can change / remove the planned session via the calendar.
 
 Please do get in touch if you need any further support in your role.
 
 Thanks,<br>
 {{ config('app.name') }}
+
+@component('mail::button', ['url' => url('/planned-session/'.$plannedSession->id )])
+Change / Delete Planned Session
+@endcomponent
 
 @component('mail::button', ['url' => url('/calendar' )])
 Calendar
