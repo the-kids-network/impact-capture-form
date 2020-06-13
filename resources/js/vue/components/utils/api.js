7b07b3c67697f7a9b22b8ea3e235e508664b7f1d
Vijay Patel <veejspeej@gmail.com>
2020-06-13 10:05:21 +0100

    fix(error-handling): Improves messaging for common API errors

--- resources/js/vue/components/utils/api.js
+++ resources/js/vue/components/utils/api.js
@@ -2,11 +2,13 @@ import _ from 'lodash'
 
 export const extractErrors = ({e, defaultMsg}) => {
     if (401 === _.get(e, 'response.status')) {
-        return [`${defaultMsg} : could not authorise the action`]
+        return [`${defaultMsg} : not authorised - do you have permission?`]
+    } else if (429 === _.get(e, 'response.status')) {
+        return [`${defaultMsg} : too many attempts - try again in around one minute` ]
     } else if (_.has(e, 'response.data.errors')) {
         const errors = _.get(e, 'response.data.errors')
         return Object.values(errors).reduce((a, b) => a.concat(b), []);
     } else {
-        return [`${defaultMsg} : unknown - contact support`]
+        return [`${defaultMsg} : unknown - contact support with page URL and screenshot`]
     }
 }
