754ce50952b9ce088f7271d24cc3adb5a64678dd
Vijay Patel <veejspeej@gmail.com>
2020-07-14 10:05:23 +0100

    chore(routing): Tidy up comments and ordering of routes

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,6 @@ Route::get('/logout', '\App\Domains\UserManagement\Controllers\LoginController@l
 Route::get('/password/reset/{token?}', '\App\Domains\UserManagement\Controllers\PasswordResetController@showResetForm')->name('password.reset');
 Route::post('/password/reset/email', '\App\Domains\UserManagement\Controllers\PasswordResetController@sendResetLinkEmail');
 Route::post('/password/reset', '\App\Domains\UserManagement\Controllers\PasswordResetController@reset');
-
 // User profile
 Route::put('/users/{user_id}/password', '\App\Domains\UserManagement\Controllers\UserPasswordController@update');
 Route::put('/users/{user_id}/contact', '\App\Domains\UserManagement\Controllers\UserContactInformationController@update');
@@ -35,20 +34,16 @@ Route::post('/users/{user_id}/photo', '\App\Domains\UserManagement\Controllers\U
 Route::delete('/users/{user_id}/photo', '\App\Domains\UserManagement\Controllers\UserPhotoController@remove');
 Route::delete('/users/{user_id}','\App\Domains\UserManagement\Controllers\UserController@delete');
 Route::post('/users/{user_id}/restore','\App\Domains\UserManagement\Controllers\UserController@restore');
-
 // User roles
 Route::put('/users/{id}/roles/{role}','\App\Domains\UserManagement\Controllers\UserRoleController@setRole');
 Route::delete('/users/{id}/roles/{role}','\App\Domains\UserManagement\Controllers\UserRoleController@removeRole');
-
 // User relationships
 Route::put('/users/{manager_id}/mentors/{mentor_id}','\App\Domains\UserManagement\Controllers\UserRelationshipController@assignMentorToManager');
 Route::put('/users/{mentor_id}/mentees/{mentee_id}','\App\Domains\UserManagement\Controllers\UserRelationshipController@assignMenteeToMentor');
 Route::delete('/users/{mentor_id}/mentees/{mentee_id}','\App\Domains\UserManagement\Controllers\UserRelationshipController@unassignMenteeFromMentor');
-
 // Mentee
 Route::post('/mentees/{id}/restore','\App\Domains\UserManagement\Controllers\MenteeController@restore');
 Route::resource('/mentees','\App\Domains\UserManagement\Controllers\MenteeController');
-
 // User management landing pages
 Route::get('/user-management/mentors','\App\Domains\UserManagement\Controllers\UserManagementPageController@mentor');
 Route::get('/user-management/managers','\App\Domains\UserManagement\Controllers\UserManagementPageController@manager');
@@ -75,27 +70,23 @@ Route::post('/emotional-states/{id}/restore/','\App\Domains\SessionReports\Contr
 // Session reports - v1
 Route::get('/report/new','\App\Domains\SessionReports\Controllers\SessionReportController@newReportForm');
 Route::post('/report', '\App\Domains\SessionReports\Controllers\SessionReportController@create');
-// session reports v2
-// redirect to vue routed app
-Route::redirect('/session-reports', '/app#/session-reports');
+// Session reports v2
+Route::redirect('/session-reports', '/app#/session-reports'); // redirect to vue routed app
 
 /**
  * Expense Claims
  */
+// Expenses v1
 Route::get('/expense-claim/export','\App\Domains\Expenses\Controllers\ExpenseClaimController@export')->name('expense-claim.export');
 Route::get('/expense-claim/new','\App\Domains\Expenses\Controllers\ExpenseClaimController@newExpenseClaim');
 Route::resource('/expense-claim','\App\Domains\Expenses\Controllers\ExpenseClaimController');
-
-Route::get('/receipts/download-all','\App\Domains\Expenses\Controllers\ReceiptController@get')->name('receipts.download-all');
-Route::get('/receipts/{id}','\App\Domains\Expenses\Controllers\ReceiptController@getById');
-
 Route::get('/finance/expense-claim/export','\App\Domains\Expenses\Controllers\FinanceController@exportExpenseClaims');
 Route::get('/finance/process-expense-claims','\App\Domains\Expenses\Controllers\FinanceController@processExpenseClaims');
-
-// expenses v2
-// redirect to vue routed app
-Route::redirect('/expenses', '/app#/expenses');
-
+// Receipts
+Route::get('/receipts/download-all','\App\Domains\Expenses\Controllers\ReceiptController@get')->name('receipts.download-all');
+Route::get('/receipts/{id}','\App\Domains\Expenses\Controllers\ReceiptController@getById');
+// Expenses v2
+Route::redirect('/expenses', '/app#/expenses'); // redirect to vue routed app
 
 /**
  * BI Reporting Routes
@@ -106,25 +97,26 @@ Route::get('/reporting/mentor/export','MentorReportingController@generateExporta
 /**
  * Calendar and events  
  */ 
+// Mentee leave
 Route::get('/mentee/leave/new','\App\Domains\Calendar\Controllers\MenteeLeaveController@newLeave');
 Route::get('/mentee/leave/{id}','\App\Domains\Calendar\Controllers\MenteeLeaveController@getOne');
 Route::post('/mentee/leave','\App\Domains\Calendar\Controllers\MenteeLeaveController@create');
 Route::put('/mentee/leave/{id}','\App\Domains\Calendar\Controllers\MenteeLeaveController@update');
 Route::delete('/mentee/leave/{id}','\App\Domains\Calendar\Controllers\MenteeLeaveController@delete');
-
+// Mentor leave
 Route::get('/mentor/leave/new','\App\Domains\Calendar\Controllers\MentorLeaveController@newLeave');
 Route::get('/mentor/leave/{id}','\App\Domains\Calendar\Controllers\MentorLeaveController@getOne');
 Route::post('/mentor/leave','\App\Domains\Calendar\Controllers\MentorLeaveController@create');
 Route::put('/mentor/leave/{id}','\App\Domains\Calendar\Controllers\MentorLeaveController@update');
 Route::delete('/mentor/leave/{id}','\App\Domains\Calendar\Controllers\MentorLeaveController@delete');
-
+// Planned session
 Route::get('/planned-session/new','\App\Domains\Calendar\Controllers\PlannedSessionController@newPlannedSession');
 Route::get('/planned-session/next','\App\Domains\Calendar\Controllers\PlannedSessionController@getNext');
 Route::get('/planned-session/{id}','\App\Domains\Calendar\Controllers\PlannedSessionController@getOne');
 Route::post('/planned-session','\App\Domains\Calendar\Controllers\PlannedSessionController@create');
 Route::put('/planned-session/{id}','\App\Domains\Calendar\Controllers\PlannedSessionController@update');
 Route::delete('/planned-session/{id}','\App\Domains\Calendar\Controllers\PlannedSessionController@delete');
-
+// Calendar
 Route::resource('/calendar','\App\Domains\Calendar\Controllers\CalendarController');
 
 /**
@@ -132,11 +124,6 @@ Route::resource('/calendar','\App\Domains\Calendar\Controllers\CalendarControlle
  */
 Route::get('/documents/upload/index','\App\Domains\Documents\Controllers\DocumentController@uploadIndex');
 Route::get('/documents/index','\App\Domains\Documents\Controllers\DocumentController@index');
-
-/**
- * Resource Tagging
- */
-
 
 /**
  * Funding
